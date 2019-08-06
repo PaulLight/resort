27e0f6a2ad8e1ebb75370efdbecf47581eee1714
paul_light <dctr.waker@gmail.com>
2019-08-06 17:31:43 +0200

    few index.html edits

--- src/App.js
+++ src/App.js
@@ -13,12 +13,14 @@ function App() {
   return (
     <>
     <Navbar/>
+    
     <Switch>
       <Route exact path="/" component={Home}/>
       <Route exact path="/rooms/" component={Rooms}/>
       <Route exact path="/rooms/:slug" component={SingleRoom}/>
       <Route component={Error}/>
     </Switch>
+    
     </>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,36 @@
-import React, { Component } from 'react'
+import React from "react";
 
-export default class Footer extends Component {
-    render() {
-        return (
-            <div>
-                
+export default function Footer() {
+  return (
+    <div>
+      <div className="main-footer">
+        <div className="container">
+          <div className="row">
+              <div className="col-md-3 cl-sm-6">
+                  <h4>Head</h4>
+                  <ul className="list-unstyled">
+                      <li>1</li>
+                      <li>1</li>
+                      <li>1</li>
+                  </ul>
+              </div>
+              <div className="col-md-3 cl-sm-6">
+                  <h4>Head</h4>
+                  <ul className="list-unstyled">
+                      <li>1</li>
+                      <li>1</li>
+                      <li>1</li>
+                  </ul>
+              </div>
+            <div className="footer-botom">
+              <p className="text-xs-center">
+                &copy;{new Date().getFullYear} Beach Shelter - All Rights
+                Reserved
+              </p>
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
